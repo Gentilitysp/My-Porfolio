383f5c62c1e7adde6995a60005cdc628f84b661c
Gentilitysp <chimaijemeze@gmail.com>
2021-08-31 15:33:54 +0100

    Add html element and scripting of the mobile-menu

---
+++ script.js
@@ -0,0 +1,31 @@
+const mobileMenuDiv = document.querySelector('#mobile-menu');
+mobileMenuDiv.className = 'mobile-menu-hidden';
+let anchorTag = document.createElement('a');
+anchorTag.setAttribute('href', '/');
+anchorTag.style.display = 'flex';
+anchorTag.style.justifyContent = 'flex-end';
+anchorTag.style.marginBottom = '10%';
+anchorTag.style.marginRight = '10%'
+let closingIcon = document.createElement('IMG');
+closingIcon.setAttribute('src', 'imgs/Icon.png');
+closingIcon.setAttribute('alt', 'icon image');
+closingIcon.id = 'close-button';
+anchorTag.appendChild(closingIcon);
+mobileMenuDiv.appendChild(anchorTag);
+anchorTag = document.createElement('a');
+anchorTag.textContent = 'Portfolio';
+anchorTag.setAttribute('href', '#actual-work-section');
+anchorTag.className = 'menu-links';
+mobileMenuDiv.appendChild(anchorTag);
+anchorTag = document.createElement('a');
+anchorTag.textContent = 'About';
+anchorTag.setAttribute('href', '#menu-about');
+anchorTag.className = 'menu-links';
+mobileMenuDiv.appendChild(anchorTag);
+anchorTag = document.createElement('a');
+anchorTag.textContent = 'Contact';
+anchorTag.setAttribute('href', '#menu-contact');
+anchorTag.className = 'menu-links';
+mobileMenuDiv.appendChild(anchorTag);
+let menuButton = document.querySelector('#menu-button');
+
