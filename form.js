d6dfc0f7bcb121d08af30e897c602a7a80c861b2
Gentilitysp <chimaijemeze@gmail.com>
2021-09-03 10:36:10 +0100

    Add Script declaration in form.js for the Form validator

---
+++ form.js
@@ -0,0 +1,8 @@
+const emailAddress = document.getElementById('user_email');
+const templateForm = document.getElementById('form');
+const validatorMessage = document.getElementById('validator-message');
+
+  } else {
+    validatorMessage.textContent = '';
+  }
+});
