4134da4f81e3b278a0cc3d61c0253cae8c32cf81
AlaaAlsalem <alsalem.aladdin@gmail.com>
2021-09-08 13:42:09 +0300

    Add functions to the mobile menu on the script.js

--- script.js
+++ script.js
@@ -35,3 +35,23 @@ mobileMenuDiv.appendChild(anchorTag);
 const menuButton = document.querySelector('#menu-button');
 
 // Declaring functions for the toggling
+
+function display() {
+    const targetedDiv = document.querySelector('.mobile-menu-hidden');
+    targetedDiv.classList.add('show');
+    menuButton.style.display = 'none';
+  }
+  menuButton.addEventListener('click', display);
+  const closeButton = document.querySelector('#close-button');
+  function closePop() {
+    const targetedDiv2 = document.querySelector('.mobile-menu-hidden');
+    targetedDiv2.classList.remove('show');
+    menuButton.style.display = 'block';
+  }
+  
+  closeButton.addEventListener('click', closePop);
+  const anchorTagLinks = document.querySelectorAll('.menu-links');
+  anchorTagLinks.forEach((link) => {
+    link.addEventListener('click', closePop);
+  });
+  
