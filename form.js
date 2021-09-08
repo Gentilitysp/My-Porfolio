48a6f198b62e9139fbc56d84c8e805be3f769a7e
Gentilitysp <chimaijemeze@gmail.com>
2021-09-08 21:07:21 +0100

    Create an empty span element to hold the validation message in html, add styles to the element in style.css, and declared the js scripting in form.js

---
+++ form.js
@@ -0,0 +1,4 @@
+const emailAddress = document.getElementById('myEmail');
+const templateForm = document.getElementById('form');
+const validatorMessage = document.getElementById('validator-message');
+
