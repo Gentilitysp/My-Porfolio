3c9ab801671b3e11d32e37a4bfbe418a5c2c9332
Gentilitysp <chimaijemeze@gmail.com>
2021-09-10 23:20:56 +0100

    Adding the local storage code tothe form.js according to my last reviewer requested changes

--- form.js
+++ form.js
@@ -9,3 +9,27 @@ templateForm.addEventListener('submit', (e) => {
     validatorMessage.textContent = '';
   }
 });
+// Start of Local Storage code
+const emailAddress = document.getElementById('mail');
+const fullName = document.getElementById('name');
+const textMessage = document.getElementById('message');
+const templateForm = document.getElementById('form');
+templateForm.addEventListener('input', () => {
+  const localStorageObject = {
+    name: fullName.value,
+    email: emailAddress.value,
+    message: textMessage.value,
+  };
+  localStorage.setItem('templateFormData', JSON.stringify(localStorageObject));
+});
+const getData = JSON.parse(localStorage.getItem('templateFormData'));
+function prePopulated() {
+  if (getData) {
+    fullName.value = getData.name;
+    emailAddress.value = getData.email;
+    textMessage.value = getData.message;
+    return getData;
+  }
+  return '';
+}
+prePopulated();
